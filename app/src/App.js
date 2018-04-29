9814f4a25a33db0eb556c951e02ec4a91c058427
@MizaelGalvez <mizaelgalvez@gmail.com>
2018-04-28 20:34:17 -0700

    empesando con create react app
    iniciando con la plantilla default

---
+++ app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './LogoADSI.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Bienvenidos a OpenADSI</h1>
+        </header>
+        <p className="App-intro">
+        Iniciemos !!!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
