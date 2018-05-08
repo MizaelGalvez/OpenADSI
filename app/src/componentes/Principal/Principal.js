ed22cc8f7dc30b881b382903552fa0b640695af4
@MizaelGalvez <mizaelgalvez@gmail.com>
2018-05-07 17:41:09 -0700

    integrando Chartjs
    intentando la Integracion con la libreria

--- app/src/componentes/Principal/Principal.js
+++ app/src/componentes/Principal/Principal.js
@@ -7,8 +7,19 @@ class Principal extends Component {
     return (
 
       <div className='Principal' style={styles.navegacion}>
-          <h3>Pagina Principal</h3>
+          <h4>Que es OpenADSI? es la herramienta perfecta para hacer crecer tu startup !!, toma deciciones, midiendo tus metricas favoritas</h4>
+          <div className="formulario-DIV">
+            <form className="formulario">
+              <div className="formulario-datos">
+                <input type="text" required placeholder="Usuario"/>
+                <input type="password" required placeholder="password"/>
+                <input type="submit" value="Enviar"/>
+              </div>
+            </form>
+            <h4>Inicia Sesion para Continuar</h4>
+          </div>
       </div>
+
 
     )
   }
--- app/src/componentes/Vista1/Vista1.js
+++ app/src/componentes/Vista1/Vista1.js
@@ -1,4 +1,42 @@
 import React, { Component } from 'react';
+import Chart from 'chart.js';
+var myChart = new Chart("myChart", {
+  type: 'bar',
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
+
 
 
 
@@ -7,8 +45,11 @@ class Vista1 extends Component {
     return (
 
       <div className='Vista1' style={styles.navegacion}>
-          <h3>Contenido Diferente</h3>
+          <div>
+          <canvas id="myChart" width="400" height="400"></canvas>
+          </div>
       </div>
+
 
     )
   }
