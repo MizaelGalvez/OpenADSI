fa5244d3fd7daf810619204196dd76df8e8e940f
xXAntonioXx <jose_antony11@hotmail.com>
2018-05-09 18:09:20 -0700

    checador
    solo faltan nombres a los botones

--- app/src/componentes/Vista4/Vista4.js
+++ app/src/componentes/Vista4/Vista4.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Clock from 'react-live-clock';
+import './estilos4.css';
 
 
 
@@ -6,8 +8,14 @@ class Vista4 extends Component {
   render() {
     return (
 
-      <div className='Vista4' style={styles.navegacion}>
-          <h3>Posible Vista de Checador</h3>
+      <div className='conten'>
+          <Clock format={'HH:mm:ss'} ticking={true} className="reloj"/>
+          <a className="relleno">relleno</a>
+          <div>
+            <div className="pic">foto</div>
+            <div className="data">nombre del empleado</div>
+            <div className="comp">complemento</div>
+          </div>
       </div>
 
     )
